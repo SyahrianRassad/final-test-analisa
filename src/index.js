60925d8f17337d57414feacda322957cda2d2e67
SyahrianRassad <syahrian_rassad_29rpl@student.smktelkom-mlg.sch.id>
2023-04-04 06:40:16 +0700

    test3

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import "bulma/css/bulma.css"
+import "./style/slick.min.css"; 
+import "./style/bulma.css"
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
