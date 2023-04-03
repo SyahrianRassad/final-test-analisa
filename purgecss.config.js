98b2d7afd3b9983fffeb7c80481555f50f9c8c4a
SyahrianRassad <syahrian_rassad_29rpl@student.smktelkom-mlg.sch.id>
2023-04-03 17:03:47 +0700

    test1

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   content: ["./src/**/*.jsx", "./src/**/*.js"],
-  css: ["./node_modules/bulma/css/bulma.css"],
-  safelist: [],
+  css: [
+    "./node_modules/bulma/css/bulma.css",
+    "./node_modules/slick-carousel/slick/slick.css",
+    "./node_modules/slick-carousel/slick/slick-theme.css"
+    ],
+  safelist: [
+    'slick-slider',
+    'slick-list',
+    'slick-track',
+    'slick-slide',
+    'slick-initialized',
+    'slick-next',
+    'slick-prev',
+    'slick-dots',
+    'slick-active'
+  ],
 };
